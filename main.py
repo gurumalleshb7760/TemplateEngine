eaf01f1ee6a9e0f6c39e5700e86f64090044fb1a
Lea Banquart <lea.banquart@uha.fr>
2019-10-24 19:08:41 +0200

    The website now allow to choose a name in a list and show it back.
    Added :
      - a CSV file with some data
      - a layout allowing easier html implementation
    
    Modified :
      - folders organization
      - all the file in order to read for a CSV and show it on website
    
    To do :
      - showing all the data from one person in the CSV, just from the name

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import pandas as pd
 
+# calling Flask API
 app = Flask(__name__)
 
+file_path = './static/data/Nom_Prenom_Age_Ville_Metier.csv'
 
+CSVFile = pd.read_csv(file_path, index_col="ID")  # stocking the data
+
+# print(CSVFile)
+
+names = [name for name in CSVFile.get('NOM')] # creating a list with all the names from the CSV file
+
+# print(names)
+
+
+# creating the index page of our website
 @app.route('/')
-def home():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", message=names)
 
 
+# creating the introduction page : NOT FINISHED YET
 @app.route('/', methods=['POST'])
 def text_box():
-    text = request.form['text']
+    text = request.form['list_name']
     processed_text = text.upper()
-    return render_template("Bienvenue.html", message=processed_text)
+    return render_template("welcome.html", message=processed_text)
 
 
 if __name__ == '__main__':
